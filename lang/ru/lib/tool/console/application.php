8f4b9783e922aba1a82d01c841a8e2d39194da83
Morozov Roman <r-r-roma-a-a@yandex.ru>
2020-12-15 16:39:04 +0300

    Логика для генерации кодов

--- lang/ru/lib/tool/console/application.php
+++ lang/ru/lib/tool/console/application.php
@@ -2,7 +2,6 @@
 $MESS['INTERVOLGA_MIGRATO.CONVERT_TO_WIN_1251'] = 'Convert output to windows-1251';
 $MESS['INTERVOLGA_MIGRATO.CONVERT_TO_UTF_8'] = 'Convert output to UTF-8';
 $MESS['INTERVOLGA_MIGRATO.SHOW_LOGS'] = 'Расширенный вывод ошибок';
-$MESS['INTERVOLGA_MIGRATO.NO_RANDOM'] = 'Удалять случайную генерацию кодов';
 
 $MESS['INTERVOLGA_MIGRATO.VERSION.MIGRATO'] = 'Версия модуля миграций: ';
 $MESS['INTERVOLGA_MIGRATO.VERSION.MAIN_MODULE'] = 'Версия главного модуля: ';
